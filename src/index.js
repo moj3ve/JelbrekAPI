2ae5210e9f8bb3112d9d3b2dc727d284b467b587
ConorTheDev <conorbyrne06@icloud.com>
2019-07-09 10:50:56 +0100

    🐛 Run on the environment variable PORT or 8080

--- src/index.js
+++ src/index.js
@@ -68,6 +68,6 @@ app.get('/v1/jailbreak/:name', (req, res) => {
 });
 
 // Start the server on port 3000!
-var server = app.listen(3000, () => {
+var server = app.listen(process.env.PORT || 8080, () => {
 	console.log(`wen eta? tfp0: 0x${server.address().port}`);
 });
